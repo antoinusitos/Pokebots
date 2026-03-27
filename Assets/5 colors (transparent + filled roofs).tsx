cbdaa9fd138f41ca8a45f743dafe64de5877075a
antoinusitos <antoine.gargasson@gmail.com>
2026-03-27 16:15:14 +0100

    Added sprites

---
+++ Assets/5 colors (transparent + filled roofs).tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="5 colors (transparent + filled roofs)" tilewidth="16" tileheight="16" tilecount="4096" columns="64">
+ <image source="../../../../Users/thebo/Downloads/12x16-retro-rpg-topdown-npc-spritesheet/5 colors (transparent + filled roofs).png" width="1024" height="1024"/>
+</tileset>
